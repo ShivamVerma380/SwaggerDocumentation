248ac030977ca8296ae6485e487224b6f53f03bb
Shivam Verma <shivam.r.verma@seagate.com>
2022-07-05 14:25:42 +0530

    Email verify api added

--- app.py
+++ app.py
@@ -7,16 +7,41 @@ async def handle(request):
     response_obj = {"Message":"Welcome"}
     return web.Response(text=json.dumps(response_obj),status = 200);
 
-async def new_user(request):
+#Verify email
+async def verifyEmail(request):
     try:
-        user = request.query['name'];
-        print("Creating a new user with name",user);
-        response_obj={"message":"User "+user+" created successfully"}
+        updateVar()
+        # flag = True;
+        email = request.query['email'];
+        response_obj = {"message":"Email verified successfully"}
         return web.Response(text=json.dumps(response_obj),status=200)
     except Exception as e:
         response_obj = {"message":str(e)}
         return web.Response(text=json.dumps(response_obj),status=500);
     
+
+
+#Register
+async def new_user(request):
+    try:
+        print(flag)
+        if flag==False:
+            response_obj = {"message":"Please verify email first"}
+            return web.Response(text=json.dumps(response_obj),status=404)
+        else:
+            user = request.query['name'];
+            print("Creating a new user with name",user);
+            response_obj={"message":"User "+user+" created successfully"}
+            return web.Response(text=json.dumps(response_obj),status=200)
+    except Exception as e:
+        response_obj = {"message":str(e)}
+        return web.Response(text=json.dumps(response_obj),status=500);
+    
+def updateVar():
+    print("In verify email")
+    global flag
+    flag = True
+    print(flag)
 # async def login(request):
 #     try:
 #         user password
@@ -27,5 +52,7 @@ async def new_user(request):
 app = web.Application();
 app.router.add_get('/',handle);
 app.router.add_post('/register',new_user);
-# app.router.add_post('/verify-otp',verifyOTP);
+flag = False
+
+app.router.add_post('/verify',verifyEmail);
 web.run_app(app)
