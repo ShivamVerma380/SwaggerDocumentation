e21ca8f8d63e1de5a419e4f8faa843b2ad703753
Shivam Verma <shivam.r.verma@seagate.com>
2022-07-09 11:12:58 +0530

    Added put api for user update

--- Test/User.py
+++ Test/User.py
@@ -26,10 +26,18 @@ async def addUser(request):
         # name = request.query["name"]
         global users
         global userId
-        users[userId] = {"email":email,"name":name}
-        userId=userId+1
-        response_obj={"Message":"User "+name+" created successfully"}
-        return web.Response(text=json.dumps(response_obj),status=200)
+        flag = True
+        for i in users:
+            if users[i].get("email")==email:
+                flag=False
+                response_obj = {"Message":"User already exists"}
+                return web.Response(text=json.dumps(response_obj),status=404)
+        if(flag):
+            users[userId] = {"email":email,"name":name}
+            userId=userId+1
+            response_obj={"Message":"User "+name+" created successfully"}
+            return web.Response(text=json.dumps(response_obj),status=200)    
+        
     except Exception as e:
         print(str(e))
         response_obj = {"Message":str(e)}
@@ -68,13 +76,36 @@ async def deleteUser(request):
         print(str(e))
         response_obj={"Message":str(e)}
         return web.Response(text=json.dumps(response_obj),status=500)
-
+    
+@swagger_path("swagger.yaml")
+async def updateUser(request):
+    try:
+        data = await request.json()
+        email = data.get("email")
+        name = data.get("name")
+        global users;
+        flag = True
+        for i in users:
+            if users[i].get("email") == email:
+                flag=False
+                users[i] = {"email":email,"name":name}
+                response_obj={"Message":"User with email "+email+ " updated successfully"}
+                return web.Response(text=json.dumps(response_obj),status=200)
+        if(flag):
+            response_obj={"Message":"User not found"}
+            return web.Response(text=json.dumps(response_obj),status=404)
+                
+    except Exception as e:
+        print(str(e))
+        response_obj={"Message":str(e)}
+        return web.Response(text=json.dumps(response_obj),status=500)
 
 
 app = web.Application()
 app.router.add_post("/user",addUser)
 app.router.add_get("/user",getUser)
 app.router.add_delete("/user",deleteUser)
+app.router.add_put("/user",updateUser)
 setup_swagger(app, swagger_url="/api/v1/doc", ui_version=2)  # <-- NEW Doc URI
 
 web.run_app(app, host="127.0.0.1")
